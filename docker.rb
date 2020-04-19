c8e9fc4b2479eae93b4b692892ffc18b907eb5ae
Bruno Kazuaki Masuda <brunokazuaki@gmail.com>
2020-04-19 11:19:34 -0300

    added auth and setting docker image

--- docker.rb
+++ docker.rb
@@ -17,4 +17,21 @@ file = File.open(gcr_auth_key, 'r')
 
 puts image_tag + image_name + docker_tls_certdir
 
-Docker.authenticate!('username' => '_json_key', 'serveraddress' => registry, 'password' => file.read)
+# Authenticating in registry
+Docker.authenticate!('username' => '_json_key',
+                     'serveraddress' => registry, 'password' => file.read)
+
+describe 'Dockerfile' do
+  before(:all) do
+    # Setting up the image to be pulled
+    @image = Docker::Image.create('fromImage' => "#{image_name}:#{image_tag}")
+
+    # Setting up serverspec environment test with a docker image
+    set :os, family: :ubuntu
+    set :backend, :docker
+    set :docker_image, @image.id
+
+    # Setting the container's entrypoint
+    set :docker_container_create_options, { 'Entrypoint' => ['sh'] }
+  end
+end
